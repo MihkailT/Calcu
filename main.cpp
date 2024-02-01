a21738e8db0469f207e5f29af8adf431d607a230
pecho <pecho@DESKTOP-TJ1JJB5>
2024-02-01 22:44:15 +0700

    Создана функция позволяющая запросить у пользователя числа для последующих операций с ними

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 #include<cmath>
 #include<stdexcept>
 
+std::pair<double, double> получить„исла() {
+    double число1, число2;
+    std::cout << "¬ведите два числа: "; // ѕросим пользовател€ ввести два числа
+    std::cin >> число1 >> число2;
+    return { число1, число2 };
+}
+
 int main() {
 
 };
