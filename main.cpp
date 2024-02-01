b266e250b878ba3d6c6a57ffdf869a4c41ac5ad2
pecho <pecho@DESKTOP-TJ1JJB5>
2024-02-01 22:56:30 +0700

    Создана функция сложения

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include<cmath>
 #include<stdexcept>
 
+// Функции для арифметических операций
+double сложение(double a, double b) {
+    return a + b; // Складываем два числа
+
 int main() {
 
 };
